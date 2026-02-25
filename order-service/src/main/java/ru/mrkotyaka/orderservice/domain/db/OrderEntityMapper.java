30c97125dfc7fe55ea882da8ff3a91414fc4414d
mrkotyaka <mrkotyaka@gmail.com>
2026-02-25 08:31:23 +0300

    refactor

--- order-service/src/main/java/ru/mrkotyaka/orderservice/domain/db/OrderEntityMapper.java
+++ order-service/src/main/java/ru/mrkotyaka/orderservice/domain/db/OrderEntityMapper.java
@@ -6,11 +6,15 @@ import ru.mrkotyaka.commonlibs.http.order.OrderDto;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface OrderEntityMapper {
+
     OrderEntity toOrderEntity(CreateOrderRequestDto request);
 
     @AfterMapping
-    default void linkOrderItemEntities(@MappingTarget OrderEntity orderEntity, OrderDto orderDto) {
-        orderEntity.getItems().forEach(orderItemEntity -> orderItemEntity.setOrder(orderEntity));
+    default void linkOrderItemEntities(@MappingTarget OrderEntity orderEntity) {
+        orderEntity
+                .getItems()
+                .forEach(
+                        orderItemEntity -> orderItemEntity.setOrder(orderEntity));
     }
 
     OrderDto toOrderDto(OrderEntity orderEntity);
