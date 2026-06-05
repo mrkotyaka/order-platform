bf94e9b413c8029d1cd114ac96283f591a32ad59
mrkotyaka <mrkotyaka@gmail.com>
2026-06-05 11:59:17 +0300

    fix customer api

--- auth-service/src/main/java/ru/mrkotyaka/authservice/api/CustomerController.java
+++ auth-service/src/main/java/ru/mrkotyaka/authservice/api/CustomerController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/customer")
+@RequestMapping("/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
 
