7add0e14ae26ea03dc909e3caa8805f32c4b9146
mrkotyaka <mrkotyaka@gmail.com>
2026-06-11 08:59:51 +0300

    feat create courier when register

--- auth-service/src/main/resources/db/changelog/changesets/001-create-users-table.sql
+++ auth-service/src/main/resources/db/changelog/changesets/001-create-users-table.sql
@@ -2,14 +2,15 @@
 --liquibase formatted sql
 
 --changeset kilianrow:1
-create table IF NOT EXISTS users
+create table if not exists users
 (
     id uuid not null primary key,
     address varchar(255),
     email varchar(255),
+    login varchar(255) not null constraint ukow0gan20590jrb00upg3va2fn unique,
+    name varchar(255),
     notification_preference varchar(255) constraint users_notification_preference_check check ((notification_preference)::text = ANY ((ARRAY ['EMAIL'::character varying, 'SMS'::character varying, 'PUSH'::character varying])::text[])),
     password varchar(255) not null,
     phone varchar(255),
-    role varchar(255) constraint users_role_check check ((role)::text = ANY ((ARRAY ['ADMIN'::character varying, 'CUSTOMER'::character varying, 'COURIER'::character varying, 'MANAGER'::character varying])::text[])),
-    username varchar(255) not null constraint ukr43af9ap4edm43mmtq01oddj6 unique
+    role varchar(255) constraint users_role_check check ((role)::text = ANY ((ARRAY ['ADMIN'::character varying, 'CUSTOMER'::character varying, 'COURIER'::character varying, 'MANAGER'::character varying])::text[]))
 );
