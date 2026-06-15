e92efd70ae630fc2d040338c1186311cf39ce355
kilian row <mrkotyaka@gmail.com>
2026-06-15 17:06:35 +0300

    fix mappers - set Mapping fields

--- delivery-service/src/main/java/ru/mrkotyaka/deliveryservice/domain/db/CourierMapper.java
+++ delivery-service/src/main/java/ru/mrkotyaka/deliveryservice/domain/db/CourierMapper.java
@@ -1,6 +1,7 @@
 package ru.mrkotyaka.deliveryservice.domain.db;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 import ru.mrkotyaka.commonlibs.dto.courier.CourierRqDto;
@@ -13,5 +14,6 @@ public interface CourierMapper {
 
     CourierEntity toCourierEntity(CourierRqDto dto);
 
+    @Mapping(source = "id", target = "courierId")
     CourierRsDto toCourierRsDto(CourierEntity entity);
 }
